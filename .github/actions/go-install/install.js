1a809ba1704d13cdcb7fa160a5a73e17b9990d21
Matt Dowdell <mdowdell244@gmail.com>
2025-01-02 12:27:26 +0000

    fix

--- .github/actions/go-install/install.js
+++ .github/actions/go-install/install.js
@@ -5,5 +5,5 @@ module.exports = async ({exec}) => {
 	const pkg = process.env.package;
 	const version = process.env.version;
 
-	await exec.exec(['go', ['install', `${pkg}@${version}`]])
+	await exec.exec('go', ['install', `${pkg}@${version}`])
 }
