2f063485f30a285a7dc24b4d795992fbefab1fdb
Matthew Dowdell <mattdowdell@users.noreply.github.com>
2025-01-08 12:14:07 +0000

    Use go-install action (#32)

--- .github/actions/go-install/install.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- */
-module.exports = async ({exec}) => {
-	const pkg = process.env.package;
-	const version = process.env.version;
-
-	await exec.exec('go', ['install', `${pkg}@${version}`])
-}
