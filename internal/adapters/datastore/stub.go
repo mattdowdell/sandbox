ad721d43b4d7a5cc152fb27afe9e7d846e01950e
Matt Dowdell <mdowdell244@gmail.com>
2025-01-24 10:06:40 +0000

    rm sleep

--- internal/adapters/datastore/stub.go
+++ internal/adapters/datastore/stub.go
@@ -3,7 +3,6 @@ package datastore
 import (
 	"context"
 	"errors"
-	"math/rand/v2"
 	"time"
 
 	"github.com/google/uuid"
@@ -18,10 +17,6 @@ func NewStub() *Stub {
 }
 
 func (s *Stub) CreateResource(context.Context, *entities.Resource) error {
-	// temporarily introduce a small sleep to generate more interesting metrics for duration
-	i := rand.IntN(1000)
-	time.Sleep(time.Millisecond * time.Duration(i))
-
 	return nil
 }
 
