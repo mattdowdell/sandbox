c621737bf4e5e369268be4d3f23a407fb7d75543
Matt Dowdell <mdowdell244@gmail.com>
2025-01-30 08:27:27 +0000

    fmt tools.go

--- tools.go
+++ tools.go
@@ -1,12 +1,13 @@
+//go:build tools
 // +build tools
 
 package main
 
 import (
-	_ "github.com/daixiang0/gci"
-	_ "mvdan.cc/gofumpt"
-	_ "github.com/vektra/mockery/v2"
 	_ "connectrpc.com/connect/cmd/protoc-gen-connect-go"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+	_ "github.com/daixiang0/gci"
 	_ "github.com/google/wire/cmd/wire"
+	_ "github.com/vektra/mockery/v2"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+	_ "mvdan.cc/gofumpt"
 )
