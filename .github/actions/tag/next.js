32a4a24a6571cf0a22a15077c91e0b47d03f9ab3
Matt Dowdell <mdowdell244@gmail.com>
2025-02-21 10:25:12 +0000

    refactor

--- .github/actions/tag/next.js
+++ .github/actions/tag/next.js
@@ -4,124 +4,89 @@
  *
  */
 module.exports = async ({ core, exec }) => {
-  const fallback = makeFallback();
+  const minimum = process.env.minimum;
+  const rollover = process.env.rollover == "true";
+  const limit = parseInt(process.env.limit);
 
-  const current = exec.getExecOutput(
+  const { describe, ok } = await current({ exec });
+
+  if (!ok || describe == minimum) {
+    core.setOutput("next", minimum);
+    return;
+  }
+
+  const next = increment({ describe, rollover, limit });
+
+  if (compare({minimum, next}) <= 0) {
+    core.setOutput("next", minimum);
+    return;
+  }
+
+  core.setOutput("next", next);
+};
+
+/**
+ * Get the current version.
+ */
+async function current({ exec }) {
+  const result = exec.getExecOutput(
     "git",
     ["describe", "--match", "'v[0-9]*"],
     { ignoreReturnCode: true },
   );
 
   if (current.exitCode != 0) {
-    core.setOutput("next", fallback);
-    return;
+    return { ok: false };
   }
 
   const describe = current.stdout.replace(/^v/, "");
-  const cmp = compare(fallback, describe);
-
-  if (cmp == -1) {
-    core.setOutput("next", fallback);
-    return;
-  }
-
-  if (cmp == 0) {
-    return;
-  }
-
-  const next = increment(describe);
-  core.setOutput("next", next);
-
-  return;
-};
-
-/**
- * Generate a fallback version in the format "YYYY.MM.0".
- */
-function makeFallback() {
-  const now = new Date(Date.now());
-
-  const year = now.getUTCFullYear();
-  const month = now.getUTCMonth() + 1;
-
-  return `${year}.${month}.0`;
+  return { describe, ok: true };
 }
 
 /**
  * Compare 2 versions together, returning one of the following integers:
  *
- * - 1: If the describe parameter is higher.
- * - 0: If the versions are the same.
- * - -1: If the fallback parameter is higher.
+ * - -1: If the minimum parameter is higher.
+ * - 0: If the values are the same.
+ * - 1: If the next parameter is higher.
  */
-function compare(fallback, describe) {
-  const desc = describe.replace(/^v/, "");
-
-  if (fallback == desc) {
+function compare({ minimum, next }) {
+  if (minimum == next) {
     return 0;
   }
 
-  const a = fallback.split(".");
-  const b = desc.split("-", 1).split(".");
+  const m = minimum.split(".").map(p => parseInt(p));
+  const n = next.split(".").map(p => parseInt(p));
 
-  for (let i = 0; i < a.length; i++) {
-    // guard against describe being shorter than fallback
-    if (i > b.length) {
+  // TODO: decide what to do if the lengths are different
+  for (let i = 0; i < m.length; i++) {
+    if (m[i] > n[i]) {
       return -1;
     }
 
-    const c = parseInt(a[i]);
-    const d = parseInt(b[i]);
-
-    if (c == d) {
-      continue;
+    if (m[i] < n[i]) {
+      return 1
     }
-
-    if (c > d) {
-      return -1;
-    }
-
-    return 1;
   }
 
-  return 1;
+  return 0;
 }
 
 /**
- * Increment the last segment of the given version.
+ * Increment a version.
  */
-function increment(version) {
-  const parts = version.split("-", 1)[0].split(".");
-  let last = parseInt(parts[parts.length - 1]);
+function increment({ describe, rollover, limit }) {
+  const parts = version.split("-", 1)[0].split(".").map(p => parseInt(p));
 
-  last += 1;
-  parts[parts.length - 1] = last.toString();
+  for (let i = parts.length - 1; i >= 0; i--) {
+    parts[i]++;
 
-  return parts.join(".");
-}
+    if (!rollover || parts[i] < limit) {
+      break;
+    }
 
-/**
- * Create the given version as an annotated git tag.
- */
-async function create(exec, version) {
-  await configure(exec);
+    parts[i] = 0;
+  }
 
-  await exec.exec("git", [
-    "tag",
-    "--annotate",
-    `v${version}`,
-    "--message",
-    `Version ${version}`,
-  ]);
-
-  // TODO: push tag
-  // await exec.exec("git", ["push", `v${version}`]);
-}
-
-/**
- *
- */
-async function configure(exec) {
-  await exec.exec("git", ["config", "user.name", "github-actions[bot]"]);
-  await exec.exec("git", ["config", "user.email", "41898282+github-actions[bot]@users.noreply.github.com"]);
+  return parts.map(p => p.toString()).join(".");
 }
