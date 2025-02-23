920fca1a200f6d0407571b64a32323ebd0579c20
Matthew Dowdell <mattdowdell@users.noreply.github.com>
2025-02-23 08:02:55 +0000

    Tag action fixes and updates (#88)
    - Add docs to tag action functions.
    - Detect rebuilds and avoid re-tagging.
    - Fix getting the current tag.

--- .github/actions/tag/next.js
+++ .github/actions/tag/next.js
@@ -1,7 +1,16 @@
 /*global module, process*/
 
 /**
+ * Calculate the next version.
  *
+ * If no tags are present, the minimum input is used. If the minimum input is greater than the
+ * calculated next tag, said minimum input is used again.
+ *
+ * The next tag is calculated by incrementing the last segment of the version. For example, "1.2.3"
+ * becomes "1.2.4". If the rollover input is enabled, then the incrementing causes the next last
+ * segment to be increased if incrementing that last segment would cause the limit to be reached.
+ * For example, with a limit of "100", "1.2.99" becomes "1.3.0". Similarly, "1.99.99" becomes
+ * "2.0.0".
  */
 module.exports = async ({ core, exec }) => {
   const minimum = process.env.minimum;
@@ -11,21 +20,27 @@ module.exports = async ({ core, exec }) => {
   const { describe, ok } = await current({ exec });
 
   if (!ok || describe == minimum) {
-    console.debug(`no existing tags, defaulting to minimum: ${minimum}`);
+    core.info(`no existing tags, defaulting to minimum: ${minimum}`);
     core.setOutput("next", minimum);
+    return;
+  }
+
+  if (!hasSuffix({ describe })) {
+    core.info(`rebuild detected, using current tag: ${describe}`);
+    core.setOutput("next", describe);
     return;
   }
 
   const next = increment({ describe, rollover, limit });
-  console.debug(`calculated next: ${next}`);
+  core.info(`calculated next: ${next}`);
 
   if (compare({ minimum, next }) <= 0) {
-    console.debug(`minimum (${minimum}) >= next (${next}), using minimum`);
+    core.info(`minimum (${minimum}) >= next (${next}), using minimum`);
     core.setOutput("next", minimum);
     return;
   }
 
-  console.debug(`next (${next}) > minimum (${minimum}), using next`);
+  core.info(`next (${next}) > minimum (${minimum}), using next`);
   core.setOutput("next", next);
 };
 
@@ -33,7 +48,7 @@ module.exports = async ({ core, exec }) => {
  * Get the current version.
  */
 async function current({ exec }) {
-  const result = exec.getExecOutput(
+  const result = await exec.getExecOutput(
     "git",
     ["describe", "--match", "v[0-9]*"],
     { ignoreReturnCode: true },
@@ -45,6 +60,35 @@ async function current({ exec }) {
 
   const describe = result.stdout.replace(/^v/, "").trim();
   return { describe, ok: true };
+}
+
+/**
+ * Test whether the describe output has a "-<count>-g<commit>" suffix.
+ */
+function hasSuffix({ describe }) {
+  return describe.split("-", 1)[0] != describe;
+}
+
+/**
+ * Increment a version.
+ */
+function increment({ describe, rollover, limit }) {
+  const parts = describe
+    .split("-", 1)[0]
+    .split(".")
+    .map((p) => parseInt(p));
+
+  for (let i = parts.length - 1; i >= 0; i--) {
+    parts[i]++;
+
+    if (!rollover || parts[i] < limit) {
+      break;
+    }
+
+    parts[i] = 0;
+  }
+
+  return parts.map((p) => p.toString()).join(".");
 }
 
 /**
@@ -74,26 +118,4 @@ function compare({ minimum, next }) {
   }
 
   return 0;
-}
-
-/**
- * Increment a version.
- */
-function increment({ describe, rollover, limit }) {
-  const parts = describe
-    .split("-", 1)[0]
-    .split(".")
-    .map((p) => parseInt(p));
-
-  for (let i = parts.length - 1; i >= 0; i--) {
-    parts[i]++;
-
-    if (!rollover || parts[i] < limit) {
-      break;
-    }
-
-    parts[i] = 0;
-  }
-
-  return parts.map((p) => p.toString()).join(".");
 }
