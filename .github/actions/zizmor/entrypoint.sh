3b9bad8c4e90a38ad16aa96d8b89fbc539700613
Matt Dowdell <mdowdell244@gmail.com>
2025-03-02 08:03:45 +0000

    Update zizmor action with feedback

--- .github/actions/zizmor/entrypoint.sh
+++ .github/actions/zizmor/entrypoint.sh
@@ -2,13 +2,4 @@
 
 set -euo pipefail
 
-export GH_TOKEN="${INPUT_GITHUB_TOKEN}"
-
-/app/zizmor \
-	--cache-dir /home/zizmor/.cache/zizmor \
-	--persona "${INPUT_PERSONA:-regular}" \
-	--format "${INPUT_FORMAT:-plain}" \
-	--min-severity "${INPUT_MIN_SEVERITY:-unknown}" \
-	--min-confidence "${INPUT_MIN_CONFIDENCE:-unknown}" \
-	--collect "${INPUT_COLLECT:-all}" \
-	${INPUT_INPUTS[@]}
+/app/zizmor ${INPUT_INPUTS[@]}
