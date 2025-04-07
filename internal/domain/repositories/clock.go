4055d9da3a0b7faff11a6418ca4234c1c88e4145
Matt Dowdell <mdowdell244@gmail.com>
2025-04-07 07:36:35 +0100

    Additional clock interface methods

--- internal/domain/repositories/clock.go
+++ internal/domain/repositories/clock.go
@@ -6,6 +6,12 @@ import (
 
 // ...
 type Clock interface {
-	// ...
+	// Now returns the current time in UTC.
 	Now() time.Time
+
+	// Since returns the time elapsed since the given value. It is shorthand for Clock.Now().Sub(t).
+	Since(t time.Time) time.Duration
+
+	// Until returns the duration until the given value. It is shorthand for t.Sub(Clock.Now()).
+	Until(t time.Time) time.Duration
 }
