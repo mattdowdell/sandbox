df4b10ca894e0e58a0751834ba45306b6ffc0d51
Matt Dowdell <mdowdell244@gmail.com>
2025-04-27 09:57:59 +0100

    use headers

--- internal/domain/repositories/doc.go
+++ internal/domain/repositories/doc.go
@@ -6,17 +6,24 @@
 // replacement with minimal changes to business logic, or even selection between multiple options at
 // runtime.
 //
+// # Specificity
+//
 // A repository should aim to be a specific and targeted interface, rather than providing generic
 // CRUDL operations. While the repository will almost certainly support CRUDL operations, a
-// repisitory should be concerned with the business process rather than the application. For
-// example, a repository may have a method for updating a specific column in a database row, despite
-// multiple columns theoretically supporting updates.
+// repisitory should be concerned with the business process rather than the application.
+//
+// For example, a repository may have a method for updating a specific column in a database row,
+// despite multiple columns theoretically supporting updates.
+//
+// # Deviations
 //
 // Some repositories might seem out of place initially, such as Clock and UUIDGenerator. These are
 // more closely aligned with good application design principles rather than Clean Architecture. They
 // are intended to abstract an external touch point of the application, namely interaction with the
 // OS, to simplify testing and ensure portability. In this case they abstract getting the system
-// time and reading random values respectively. This lack of following a rigid adherance is a good
-// example of purism to the detriment of the application. Deviations are acceptable if they can be
-// justified, and fundamental assumptions such as direction of dependencies are not broken.
+// time and reading random values respectively.
+//
+// This lack of following a rigid adherance is a good example of purism to the detriment of the
+// application. Deviations are acceptable if they can be justified and fundamental assumptions, such
+// as direction of dependencies, are not broken.
 package repositories
