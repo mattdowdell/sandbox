9fad306601d30a0932d8dc42b34d79463f4f823d
Matt Dowdell <mdowdell244@gmail.com>
2025-05-10 08:36:26 +0100

    Add drivers docs

--- internal/drivers/clock/clock.go
+++ internal/drivers/clock/clock.go
@@ -6,28 +6,29 @@ import (
 	"github.com/mattdowdell/sandbox/internal/domain/repositories"
 )
 
-// ...
+// Non-allocating compile-time check for interface compliance.
 var _ repositories.Clock = (*Clock)(nil)
 
-// ...
+// Clock provides a wrapper around functions in [time] to support dependency injection and mocking
+// in unit tests.
 type Clock struct{}
 
-// ...
+// New creates a new Clock.
 func New() *Clock {
 	return &Clock{}
 }
 
-// ...
+// Now returns the current time in UTC.
 func (c *Clock) Now() time.Time {
 	return time.Now().UTC()
 }
 
-// ...
+// Since returns the time elapsed since the given value.
 func (c *Clock) Since(t time.Time) time.Duration {
 	return c.Now().Sub(t)
 }
 
-// ...
+// Until returns the duration until the given value.
 func (c *Clock) Until(t time.Time) time.Duration {
 	return t.Sub(c.Now())
 }
