27066b5f20ef982fde920d5873d204e0d1d1eeb4
Matt Dowdell <mdowdell244@gmail.com>
2025-06-02 09:23:19 +0100

    add flagoptions tests

--- internal/drivers/config/flagoptions/options.go
+++ internal/drivers/config/flagoptions/options.go
@@ -9,38 +9,71 @@ import (
 	"github.com/mattdowdell/sandbox/internal/drivers/config"
 )
 
-// Command-line flags for bootstrapping the service configuration collection.
-var (
-	envPrefix = flag.String(
-		"config.envprefix",
-		envOrDefault("CONFIG_ENVPREFIX", "APP_"),
-		"The environment variable prefix to filter by for configuration.",
-	)
-	files = flag.String(
-		"config.files",
-		os.Getenv("CONFIG_FILES"),
-		"The file paths to use for configuration.",
-	)
-	mounts = flag.String(
-		"config.mounts",
-		os.Getenv("CONFIG_MOUNTS"),
-		"The kubernetes pod mounts to use for configuration.",
-	)
-)
-
-// New creates a new Options instance populated with value from CLI flags.
+// New creates a new Options instance populated with values from CLI flags.
 //
 // - -config.envprefix is the prefix for environment variables to read configuration from.
 // - -config.files is a comma delimited list of files to read configuration from.
 // - -config.mounts is a comma delimited list of kubernetes pod mounts to read configuration from.
 func New() *config.Options {
-	flag.Parse()
+	// copied from flag, apparently execl can cause os.Args to be empty
+	var name string
+	if len(os.Args) > 0 {
+		name = os.Args[0]
+	}
+
+	// use a flagset instead of flag.String, etc. so that multiple calls don't cause an error.
+	flagset := flag.NewFlagSet(name, flag.ExitOnError)
+
+	options, _ := NewWithFlagSet(flagset)
+	return options
+}
+
+// NewWithFlagSet creates a new Options instance populated with values from CLI flags.
+//
+// - -config.envprefix is the prefix for environment variables to read configuration from.
+// - -config.files is a comma delimited list of files to read configuration from.
+// - -config.mounts is a comma delimited list of kubernetes pod mounts to read configuration from.
+//
+// New creates a separate [flag.FlagSet] instance which is appropriate for normal binaries. However,
+// this separation means that flags defined with [flag.String], etc. in other packages will result
+// in an error because they're unknown. This can happen during tests. To enable use of other flags,
+// the default flagset can be passed here instead:
+//
+//	// ignore errors, configured with flag.ExitOnError
+//	options, _ := flagoptions.NewWithFlagSet(flag.Commandline)
+func NewWithFlagSet(flagset *flag.FlagSet) (*config.Options, error) {
+	// define flags within the function so environment variables can var across unit tests
+	envPrefix := flagset.String(
+		"config.envprefix",
+		envOrDefault("CONFIG_ENVPREFIX", "APP_"),
+		"The environment variable prefix to filter by for configuration.",
+	)
+	files := flagset.String(
+		"config.files",
+		os.Getenv("CONFIG_FILES"),
+		"The file paths to use for configuration.",
+	)
+	mounts := flagset.String(
+		"config.mounts",
+		os.Getenv("CONFIG_MOUNTS"),
+		"The kubernetes pod mounts to use for configuration.",
+	)
+
+	// apparently execl can cause os.Args to be empty
+	var args []string
+	if len(os.Args) > 1 {
+		args = os.Args[1:]
+	}
+
+	if err := flagset.Parse(args); err != nil {
+		return nil, err
+	}
 
 	return &config.Options{
 		EnvPrefix: *envPrefix,
 		Files:     splitPaths(*files),
 		Mounts:    splitPaths(*mounts),
-	}
+	}, nil
 }
 
 // envOrDefault reads the value of the given environment variable, using the given fallback of the
--- internal/drivers/config/flagoptions/options_test.go
+++ internal/drivers/config/flagoptions/options_test.go
@@ -1,7 +1,139 @@
 package flagoptions_test
 
 import (
+	"flag"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/mattdowdell/sandbox/internal/drivers/config"
+	"github.com/mattdowdell/sandbox/internal/drivers/config/flagoptions"
 )
 
-func Test_New(_ *testing.T) {}
+func setArgs(t *testing.T, args ...string) {
+	t.Helper()
+
+	old := os.Args
+	os.Args = args
+
+	t.Cleanup(func() {
+		os.Args = old
+	})
+}
+
+func Test_New(t *testing.T) {
+	testCases := []struct {
+		name string
+		args []string
+		env  map[string]string
+		want *config.Options
+	}{
+		{
+			name: "defaults",
+			args: []string{"example"},
+			want: &config.Options{
+				EnvPrefix: "APP_",
+				Files:     []string{},
+				Mounts:    []string{},
+			},
+		},
+		{
+			name: "arguments",
+			args: []string{
+				"example",
+				"-config.envprefix=ARG_",
+				"-config.files=/etc/arg1,/etc/arg2",
+				"-config.mounts=/etc/arg3,/etc/arg4",
+			},
+			want: &config.Options{
+				EnvPrefix: "ARG_",
+				Files:     []string{"/etc/arg1", "/etc/arg2"},
+				Mounts:    []string{"/etc/arg3", "/etc/arg4"},
+			},
+		},
+		{
+			name: "environment",
+			env: map[string]string{
+				"CONFIG_ENVPREFIX": "ENV_",
+				"CONFIG_FILES":     "/etc/env1,/etc/env2",
+				"CONFIG_MOUNTS":    "/etc/env3,/etc/env4",
+			},
+			want: &config.Options{
+				EnvPrefix: "ENV_",
+				Files:     []string{"/etc/env1", "/etc/env2"},
+				Mounts:    []string{"/etc/env3", "/etc/env4"},
+			},
+		},
+		{
+			name: "both",
+			args: []string{
+				"example",
+				"-config.envprefix=ARG_",
+				"-config.files=/etc/arg1,/etc/arg2",
+				"-config.mounts=/etc/arg3,/etc/arg4",
+			},
+			env: map[string]string{
+				"CONFIG_ENVPREFIX": "ENV_",
+				"CONFIG_FILES":     "/etc/env1,/etc/env2",
+				"CONFIG_MOUNTS":    "/etc/env3,/etc/env4",
+			},
+			want: &config.Options{
+				EnvPrefix: "ARG_",
+				Files:     []string{"/etc/arg1", "/etc/arg2"},
+				Mounts:    []string{"/etc/arg3", "/etc/arg4"},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// arrange
+			setArgs(t, tc.args...)
+
+			for key, val := range tc.env {
+				t.Setenv(key, val)
+			}
+
+			// act
+			got := flagoptions.New()
+
+			// assert
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_NewWithFlagSet_Success(t *testing.T) {
+	// arrange
+	flagset := flag.NewFlagSet(t.Name(), flag.ContinueOnError)
+
+	setArgs(t, "example")
+
+	// act
+	got, err := flagoptions.NewWithFlagSet(flagset)
+
+	// assert
+	want := &config.Options{
+		EnvPrefix: "APP_",
+		Files:     []string{},
+		Mounts:    []string{},
+	}
+
+	assert.Equal(t, want, got)
+	assert.NoError(t, err)
+}
+
+func Test_NewWithFlagSet_Error(t *testing.T) {
+	// arrange
+	flagset := flag.NewFlagSet(t.Name(), flag.ContinueOnError)
+
+	setArgs(t, "example", "-invalid=invalid")
+
+	// act
+	got, err := flagoptions.NewWithFlagSet(flagset)
+
+	// assert
+	assert.Nil(t, got)
+	assert.EqualError(t, err, "flag provided but not defined: -invalid")
+}
