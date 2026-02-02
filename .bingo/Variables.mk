54429f1936a204d6af79a4c145f46bffd8324027
Matthew Dowdell <mattdowdell@users.noreply.github.com>
2026-02-02 08:36:01 +0000

    Manage Trivy with Bingo (#628)
    Fixes #390.

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -83,6 +83,12 @@ $(PROTOC_GEN_GO): $(BINGO_DIR)/protoc-gen-go.mod
 	@echo "(re)installing $(GOBIN)/protoc-gen-go-v1.36.11"
 	@cd $(BINGO_DIR) && GOWORK=off GOOS=$(GOHOSTOS) GOARCH=$(GOHOSTARCH) GOARM=$(GOHOSTARM) $(GO) build -mod=mod -modfile=protoc-gen-go.mod -o=$(GOBIN)/protoc-gen-go-v1.36.11 "google.golang.org/protobuf/cmd/protoc-gen-go"
 
+TRIVY := $(GOBIN)/trivy-v0.67.2
+$(TRIVY): $(BINGO_DIR)/trivy.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/trivy-v0.67.2"
+	@cd $(BINGO_DIR) && GOWORK=off GOOS=$(GOHOSTOS) GOARCH=$(GOHOSTARCH) GOARM=$(GOHOSTARM) GOEXPERIMENT=jsonv2 $(GO) build -mod=mod -modfile=trivy.mod -o=$(GOBIN)/trivy-v0.67.2 "github.com/aquasecurity/trivy/cmd/trivy"
+
 YAMLFMT := $(GOBIN)/yamlfmt-v0.21.0
 $(YAMLFMT): $(BINGO_DIR)/yamlfmt.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
