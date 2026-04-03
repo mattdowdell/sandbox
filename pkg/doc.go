dba64064053f3be3f7de81b4d1e87b5421096086
Matt Dowdell <mdowdell244@gmail.com>
2026-04-03 10:22:56 +0100

    Add package-level docs to pkg/

---
+++ pkg/doc.go
@@ -0,0 +1,3 @@
+// Package pkg contains mature and re-usable packages that could be used in other projects. In the
+// future, they may be moved to a separate repository.
+package pkg
---
+++ pkg/slogx/doc.go
@@ -0,0 +1,2 @@
+// Package slogx provides additional helpers for [log/slog] consumers.
+package slogx
